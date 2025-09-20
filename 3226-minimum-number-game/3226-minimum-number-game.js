5521632547f813a909cd7c8cccb7b7a31484de5e
imsanupm <sanukrishna23@gmail.com>
2025-09-21 00:27:28 +0530

    Time: 2 ms (87.46%), Space: 58.2 MB (26.04%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.js
+++ 3226-minimum-number-game/3226-minimum-number-game.js
@@ -3,28 +3,24 @@
  * @return {number[]}
  */
 var numberGame = function(nums) {
-    let arr = [];
+    let arr=[]
 
-    while (nums.length > 0) {
-        let smallest = Infinity;
-        let second = Infinity;
+  while(nums.length>0){
+    let smallest=Infinity
+    let second=Infinity
 
-        for (let i = 0; i < nums.length; i++) {
-            if (nums[i] < smallest) {
-                second = smallest;
-                smallest = nums[i];
-            } else if (nums[i] < second) {
-                second = nums[i];
-            }
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]<smallest){
+            second=smallest
+            smallest=nums[i]
+        }else if(nums[i]<second){
+            second=nums[i]
         }
-
-
-        nums.splice(nums.indexOf(smallest), 1);
-        nums.splice(nums.indexOf(second), 1);
-
-
-        arr.push(second, smallest);
     }
-
-    return arr;
+    nums.splice(nums.indexOf(second),1)
+    nums.splice(nums.indexOf(smallest),1)
+    arr.push(second,smallest)
+  }
+  return arr
+  
 };
