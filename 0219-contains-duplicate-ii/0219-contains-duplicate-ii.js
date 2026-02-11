13ad99533865130585ea7acd0046bd24b2796115
imsanupm <sanukrishna23@gmail.com>
2026-02-11 09:37:06 +0530

    Time: 36 ms (33%), Space: 85.6 MB (23.12%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const map = new Map(); 
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+
+        if (map.has(num)) {
+            if (i - map.get(num) <= k) {
+                return true;
+            }
+        }
+
+       
+        map.set(num, i);
+    }
+
+    return false;
+  
+};
+
