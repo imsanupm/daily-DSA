94264eabe1d89947e37b26d6096bfc2d2f68d7bc
imsanupm <sanukrishna23@gmail.com>
2026-04-10 09:11:38 +0530

    Time: 0 ms (100%), Space: 54.2 MB (87.83%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.ts
@@ -0,0 +1,14 @@
+function removeElement(nums: number[], val: number): number {
+    
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i]==val){
+            nums.splice(i,1);
+            i--
+        }
+    };
+
+    if(nums.length !=  val){
+        return nums.length
+    }
+    
+};
