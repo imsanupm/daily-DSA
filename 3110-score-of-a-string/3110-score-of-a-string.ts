78f8e121790ff60c40aa118c5cf8b6462b5a5fab
imsanupm <sanukrishna23@gmail.com>
2026-04-12 12:29:39 +0530

    Time: 0 ms (100%), Space: 57.7 MB (68.09%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.ts
@@ -0,0 +1,12 @@
+function scoreOfString(s: string): number {
+    
+    let result = 0;
+
+    for(let i = 0 ; i< s.length-1; i++){
+        result += Math.abs(
+            s.charCodeAt(i) - s.charCodeAt(i+1)
+        )
+    }
+
+    return result
+};
