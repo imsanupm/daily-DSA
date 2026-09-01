47358a39502fc0d02379c515c1f14172be188854
Sanukrishna PM <sanukrishna23@gmail.com>
2026-09-01 22:09:33 +0530

    Time: 0 ms (100%), Space: 54 MB (86%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let set  = new Set(nums1);
+    let result = new Set()
+    for(let i = 0; i< nums2.length; i++){
+        if(set.has(nums2[i])){
+            result.add(nums2[i])
+        }
+    };
+    
+    return [...result]
+};
